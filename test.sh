60dd66dac182643859a338629ebee486e5cfbbcb
Elan Ruusamäe <glen@delfi.ee>
2013-03-08 11:55:13 +0200

    add script to test known domains

---
+++ test.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+if [ ! -e utils.sh ]; then
+	cat > utils.sh <<-EOF
+	STATE_OK=0
+	STATE_WARNING=1
+	STATE_CRITICAL=2
+	STATE_UNKNOWN=3
+	STATE_DEPENDENT=4
+	EOF
+fi
+
+
+domains="
+mail.ru
+delfi.ee
+delfi.tv
+amazon.ca
+amazon.ie
+amazon.co.uk
+"
+
+for domain in $domains; do
+	sh -$- ./check_domain -d $domain
+done
